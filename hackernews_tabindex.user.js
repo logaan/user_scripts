529b7b7d0f45967865739569480ea46441875691
Colin Campbell-McPherson <colin@logaan.net>
2010-02-10 01:03:41 +0800

    corrects tab order focusses on first link moves through links as clicked

---
+++ hackernews_tabindex.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name           Hacker News Tab Index
+// @namespace      http://logaan.net/
+// @include        http://news.ycombinator.com/
+// ==/UserScript==
+
+(function() {
+  // Have to use a factory for this to get the scoping right. Unfortunately JS
+  // re-uses the index variable in the for loop so having the callback inline
+  // breaks. Also we've got to explicitly parse the index as an int as it's being
+  // given to us as a string. Took a while to figure out that one.
+  function focus_next_factory(index){
+    return function focus_next() {
+      document.getElementsByClassName("title")[parseInt(index) + 2].children[0].focus();
+    };
+  }
+
+  var title_cells = document.getElementsByClassName("title");
+  var link_count = 0;
+
+  for(index in title_cells) {
+    if(index % 2 == 1) {
+      // Set the tabindex to something more helpful
+      link_count = link_count + 1;
+      title_cells[index].children[0].tabIndex = link_count;
+
+      // Focus on the next link when this one is clicked
+      title_cells[index].children[0].addEventListener("click", focus_next_factory(index), true);
+    }
+  }
+
+  // Focus on the first story
+  title_cells[1].children[0].focus();
+}())
+
